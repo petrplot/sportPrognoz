53a2998f5da01baf2a18fbee716e6336aa1ed609
petrplot <petr.plotnikov.1989@mail.ru>
2021-05-08 16:13:44 +0700

    create method register

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,44 @@
+const errorHandler = require('../utilities/errorHandler')
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
 
+module.exports.register = async(req,res)=>{
 
-module.exports.register = async()=>{
+	//find of user by email
+	const candidate = await User.findOne({email:req.body.email})
+	
+	if (candidate) {
 
+		res.status(409).json({
+			message: "Данный email уже занят!"
+		})
+
+	} else {
+		
+		//create user
+		const salt = bcrypt.genSaltSync(10)
+		const password = req.body.password
+		const user = new User({
+
+			name: req.body.name,
+			email: req.body.email,
+			password: bcrypt.hashSync(password, salt)
+
+		})
+
+		try {
+
+			await user.save()
+			res.status(201).json(user, console.log("Пользователь создан!"))
+			
+		} catch (e) {
+
+			errorHandler(res, e)
+			
+		}
+	}
 }
+
 module.exports.login = async()=>{
 	
 }
