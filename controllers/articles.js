8c222cadeda2b0028d0dc1d44074849180b858e7
petrplot <petr.plotnikov.1989@mail.ru>
2021-05-11 16:08:26 +0700

    created method Article create

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,7 +1,29 @@
+const Article = require('../models/Article')
+const errorHandler = require('../utilities/errorHandler')
+
 module.exports.getByArticleId = async()=>{
 
 }
-module.exports.create = async()=>{
+module.exports.create = async(req, res)=>{
+
+	const article = new Article({
+		title: req.body.title,
+		text: req.body.text,
+		description: req.body.description,
+		author: req.body.author ? req.body.author : author.default,
+		imageSrc: req.file ? req.file.path : ''
+	})
+
+	try {
+
+		await article.save()
+		res.json(201, article)
+		
+	} catch (e) {
+
+		errorHandler(res, e)
+
+	}
 
 }
 module.exports.update = async()=>{
--- routes/article.js
+++ routes/article.js
@@ -2,10 +2,11 @@ const {Router} = require('express')
 const router = Router()
 
 const controller = require('../controllers/articles')
+const upload = require('../middleware/upload')
 
 router.get('/:id', controller.getByArticleId)
-router.post('/', controller.create)
-router.patch('/:id', controller.update)
+router.post('/', upload.single('image'), controller.create)
+router.patch('/:id', upload.single('image'), controller.update)
 router.delete('/:id', controller.remove)
 
 module.exports = router
