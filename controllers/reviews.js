6cdf4042e33391293a95f6e119bd4e9ab9ff738d
petrplot <petr.plotnikov.1989@mail.ru>
2021-05-16 15:20:10 +0700

    created methods for review

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,12 +1,74 @@
-module.exports.getByReviewId = async()=>{
+const Review =require('../models/Review')
 
+module.exports.getAll = async(req, res)=>{
+	try {
+		const reviews = await Review.find()
+		res.status(201).json(reviews)
+	} catch (e) {
+		errorHandler(res, e)
+	}
 }
-module.exports.create = async()=>{
 
+module.exports.getById = async(req, res)=>{
+	try {
+		const review = await Review.findById(req.params.id)
+		res.status(201).json(review)
+	} catch (e) {
+		errorHandler(res, e)
+	}
 }
-module.exports.update = async()=>{
 
+module.exports.create = async(req, res)=>{	
+	try {
+		const review = new Review({
+			imageSrc:req.file?req.file.path:"",
+			text:req.body.text,
+		})
+	await review.save()
+	res.status(201).json(review)
+	} catch (e) {
+		errorHandler(res, e)
+	}
 }
-module.exports.remove = async()=>{
+
+ module.exports.update = async(req, res)=>{
+	
+	try {
+		const updated = req.body
+
+	if(!updated._id){
+		console.log(" id не указан")
+	}
+
+	if (req.file) {
+		updated.imageSrc = req.file.path
+	}
+
+	const review = await Review.findByIdAndUpdate(
+		updated._id, 
+		updated,
+		{new:true},
+		function(err, review){
+			if(err) return console.log(err)
+			console.log("Обновленный объект", review)
+		}
+	)
+		res.status(200).json(review)
+	} catch (error) {
+		errorHandler(res, error)
+	}
+}
+
+module.exports.remove = async(req, res)=>{
+	try {
+		const{id} = req.params
+		const review = await Review.findByIdAndDelete(id)
+
+		res.status(200).json({
+			message: 'Статья удалена.', review
+		})	
+	} catch (e) {
+		errorHandler(res, e)
+	}
 
 }
--- routes/reviews.js
+++ routes/reviews.js
@@ -2,9 +2,10 @@ const {Router} = require('express')
 const router = Router()
 const controller = require('../controllers/reviews')
 
-router.get('/:reviewId',controller.getByReviewId)
-router.post('/', controller.create)
-router.patch('/:id', controller.update)
-router.delete('/:id', controller.remove)
+router.get('/:Id', controller.getById)
+router.get('/', controller.getAll)
+router.post('/',passport.authenticate('jwt', { session: false }), upload.single('image'), controller.create)
+router.put('/update',passport.authenticate('jwt', { session: false }), upload.single('image'), controller.update)
+router.delete('/:id', passport.authenticate('jwt', { session: false }), controller.remove)
 
 module.exports = router
